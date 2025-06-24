b3efb46d9d7dbfa39cab23450a8b28797f228112
AryCord26 <ary_cordeiro@outlook.com>
2025-06-24 08:19:44 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const fadeInElements = document.querySelectorAll('.fade-in');
+  fadeInElements.forEach(el => {
+    el.classList.add('show');
+  });
+});
 
